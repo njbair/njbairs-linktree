b1a99d0f16ac3693a36bdc1f47aba07d5ca472de
Nick Bair <njbair@gmail.com>
2022-09-09 12:33:49 -0400

    deploy

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+HUGO_VERSION=$(cat .hugo_version)
+
+curl -sL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz | tar xz hugo
+./hugo
+
+rm hugo
