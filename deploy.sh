daa09c0d50950477230acc8c2cb103f1bd71b650
Nick Bair <njbair@gmail.com>
2022-10-21 16:08:10 -0400

    bug

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+npm install
+
 HUGO_VERSION=$(cat .hugo_version)
 
 curl -sL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz | tar xz hugo
