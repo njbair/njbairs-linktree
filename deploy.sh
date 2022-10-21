9df72b9210c887eb4105063ee3839c09c47f859f
Nick Bair <njbair@gmail.com>
2022-10-21 16:12:35 -0400

    hugo version

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ npm install
 
 HUGO_VERSION=$(cat .hugo_version)
 
-curl -sL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz | tar xz hugo
+curl -sL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz | tar xz hugo
 ./hugo
 
 rm hugo
